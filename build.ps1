d5aa9d089574ba5ed154f6fe4c2207708fe2d146
Wil Taylor <wilfridtaylor@gmail.com>
2017-05-03 09:49:23 +1000

    Added support for dev myget feed

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
-$apikey = $env:poshapikey
+$apikey = $env:apikey
 $version = $env:APPVEYOR_BUILD_VERSION
+$publishUrl = $env:poshurl
 
 New-Item "$HOME\Documents\WindowsPowerShell\Modules" -Force -ErrorAction SilentlyContinue -ItemType Directory
 Copy-Item $PSScriptRoot "$HOME\Documents\WindowsPowerShell\Modules" -Force -Recurse -Exclude '.git\*.*', 'build.ps1', "README.md"
@@ -7,4 +8,9 @@ Remove-Item "$HOME\Documents\WindowsPowerShell\Modules\AtlasBox\.git" -Force -Re
 
 (Get-Content -Path "$PSScriptRoot\AtlasBox.psd1") -replace "%BUILDVERSION%", $version | Out-File "$HOME\Documents\WindowsPowerShell\Modules\AtlasBox\AtlasBox.psd1"
 
-Publish-Module -Name AtlasBox -NuGetApiKey $apikey
+if([string]::IsNullOrEmpty($publishUrl)) {
+    Publish-Module -Name AtlasBox -NuGetApiKey $apikey -Verbose
+}else{
+    Register-PSRepository -Name poshdev -SourceLocation $publishUrl -PublishLocation $publishUrl
+    Publish-Module -Name AtlasBox -NuGetApiKey $apikey -Repository poshdev -Verbose
+}
